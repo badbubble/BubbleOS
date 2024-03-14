33950c84de83ba0d657fc3458387e7d59355eb8c
badbubble <liuhanjun369@gmail.com>
2024-03-14 19:07:03 +0100

    hello BubbleOS

---
+++ kernel.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void start_kernel(void)
+{
+    while (1)
+    {
+        printf("Hello, BubbleOS")
+    };
+}
---
+++ platform.h
@@ -0,0 +1,14 @@
+#ifndef __PLATFORM_H__
+#define __PLATFORM_H__
+
+/*
+ * QEMU RISC-V Virt machine with 16550a UART and VirtIO MMIO
+ */
+
+/* 
+ * maximum number of CPUs
+ * #define VIRT_CPUS_MAX 8
+ */
+#define MAXNUM_CPU 8
+
+#endif /* __PLATFORM_H__ */
