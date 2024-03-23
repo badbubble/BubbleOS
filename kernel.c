768cd269e814bfb5d656b65b673468f31ab3d515
badbubble <liuhanjun369@gmail.com>
2024-03-23 18:37:45 +0100

    init page

--- kernel.c
+++ kernel.c
@@ -1,13 +1,16 @@
+#include "os.h"
+
 extern void uart_init(void);
-extern void uart_puts(char *s);
-
-
+extern void page_init(void);
+extern void page_test(void);
 void start_kernel(void)
 {
     uart_init();
-	uart_puts("Hello, BubbleOS!\n");
+    page_init();
+    uart_puts("-----------------------------\n");
+    uart_puts("Hello, BubbleOS!\n");
+
     while (1)
     {
-
     }
 }
