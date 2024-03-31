1253051ba491e3b3b8b639d195b9118c857b15fd
badbubble <liuhanjun369@gmail.com>
2024-03-31 18:19:19 +0000

    define user's programs

---
+++ user.c
@@ -0,0 +1,33 @@
+#include "os.h"
+
+#define DELAY 1000
+
+void user_task0(void)
+{
+    uart_puts("Task 0 created!\n");
+    while(1)
+    {
+        uart_puts("Task 0 running...\n");
+        task_delay(DELAY);
+        task_yield();
+    }
+}
+
+void user_task1(void)
+{
+    printf("Task 1 created!\n");
+    while(1)
+    {
+        uart_puts("Task 1 running...\n");
+        task_delay(DELAY);
+        task_yield();
+    }
+}
+
+void os_main(void)
+{
+	task_create(user_task0);
+	task_create(user_task1);
+}
+
+
