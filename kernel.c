dce4c03a1ab77fc6949bb158f44e8272044f2345
badbubble <liuhanjun369@gmail.com>
2024-03-31 18:20:39 +0000

    multi-task schedule

--- kernel.c
+++ kernel.c
@@ -3,13 +3,25 @@
 extern void uart_init(void);
 extern void page_init(void);
 extern void page_test(void);
+extern void sched_init(void);
+extern void schedule(void);
+extern void os_main(void);
+
 void start_kernel(void)
 {
     uart_init();
-    page_init();
     uart_puts("-----------------------------\n");
     uart_puts("Hello, BubbleOS!\n");
+    uart_puts("-----------------------------\n");
+    
+    page_init();
 
+    sched_init();
+
+    os_main();
+
+    schedule();
+    
     while (1)
     {
     }
