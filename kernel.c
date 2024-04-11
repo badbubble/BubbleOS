30c1e36d127b9fb15636b13b9161660cef4caafa
badbubble <liuhanjun369@gmail.com>
2024-04-11 21:40:14 +0200

    insert trap_init

--- kernel.c
+++ kernel.c
@@ -6,6 +6,7 @@ extern void page_test(void);
 extern void sched_init(void);
 extern void schedule(void);
 extern void os_main(void);
+extern void trap_init(void);
 
 void start_kernel(void)
 {
@@ -13,15 +14,17 @@ void start_kernel(void)
     uart_puts("-----------------------------\n");
     uart_puts("Hello, BubbleOS!\n");
     uart_puts("-----------------------------\n");
-    
+
     page_init();
+
+    trap_init();
 
     sched_init();
 
     os_main();
 
     schedule();
-    
+
     while (1)
     {
     }
