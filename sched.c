c7a1aefaf174df5a4e5b248604c8222a1ef0a97d
badbubble <liuhanjun369@gmail.com>
2024-04-11 21:41:12 +0200

    remove duplicated

--- sched.c
+++ sched.c
@@ -13,13 +13,8 @@ extern void switch_to(struct context *next);
 uint8_t __attribute__((aligned(16))) task_stack[MAX_TASKS][STACK_SIZE];
 struct context ctx_tasks[MAX_TASKS];
 
-static int _top = 0; // current number of task
+static int _top = 0;      // current number of task
 static int _current = -1; // point to the context of current task
-
-static void w_mscratch(reg_t x)
-{
-    asm volatile("csrw mscratch, %0" : : "r" (x));
-}
 
 void user_task0(void);
 
@@ -30,9 +25,10 @@ void sched_init()
 
 void schedule()
 {
-    if (_top <= 0) {
+    if (_top <= 0)
+    {
         panic("Num of task should be greater than zero!");
-		return;
+        return;
     }
     _current = (_current + 1) % _top;
 
@@ -42,9 +38,10 @@ void schedule()
 
 int task_create(void (*start_routin)(void))
 {
-    if (_top < MAX_TASKS) {
-        ctx_tasks[_top].sp = (reg_t) &task_stack[_top][STACK_SIZE];
-        ctx_tasks[_top].ra = (reg_t) start_routin;
+    if (_top < MAX_TASKS)
+    {
+        ctx_tasks[_top].sp = (reg_t)&task_stack[_top][STACK_SIZE];
+        ctx_tasks[_top].ra = (reg_t)start_routin;
         _top++;
         return 0;
     }
@@ -58,6 +55,7 @@ void task_yield()
 
 void task_delay(volatile int count)
 {
-	count *= 50000;
-	while (count--);
+    count *= 50000;
+    while (count--)
+        ;
 }
