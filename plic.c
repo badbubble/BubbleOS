7d0cbab605718e460f5132adec9686177de035ac
badbubble <liuhanjun369@gmail.com>
2024-04-14 14:56:43 +0200

    basic plic functions

---
+++ plic.c
@@ -0,0 +1,28 @@
+#include "os.h"
+
+void plic_init(void)
+{
+    int hart = r_tp();
+
+    *(uint32_t *) PLIC_PRIORITY(UART0_IRQ) = 1;
+    *(uint32_t *) PLIC_MENABLE(hart, UART0_IRQ) = (1 << (UART0_IRQ % 32));
+    // allow all passing
+    *(uint32_t*)PLIC_MTHRESHOLD(hart) = 0;
+    // allow external interrupts
+    w_mie(r_mie() | MIE_MEIE);
+    // allow global interrupts
+    w_mstatus(r_mstatus() | MSTATUS_MIE);
+}
+
+int plic_claim(void)
+{
+    int hart = r_tp();
+    int irq = *(uint32_t*)PLIC_MCLAIM(hart);
+    return irq;
+}
+
+void plic_complete(int irq)
+{
+    int hart = r_tp();
+    *(uint32_t*)PLIC_MCOMPLETE(hart) = irq;
+}
