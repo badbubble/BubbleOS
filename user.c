9c823b9efd720673e208e4065ee60f83e006b6b8
badbubble <liuhanjun369@gmail.com>
2024-04-16 09:20:29 +0200

    using timer in user program

--- user.c
+++ user.c
@@ -1,32 +1,65 @@
 #include "os.h"
 
-#define DELAY 100000
+#define DELAY 10000
 
 #define USE_LOCK
 
+struct userdata {
+	int counter;
+	char *str;
+};
+
+struct userdata person = {0, "BadBubble"};
+
+void timer_func(void *arg)
+{
+    if (arg == NULL)
+    {
+        return;
+    }
+    
+    struct userdata *param = (struct userdata *)arg;
+
+	param->counter++;
+	printf("======> TIMEOUT: %s: %d\n", param->str, param->counter);
+}
 
 void user_task0(void)
 {
     uart_puts("[Task 0] Created!\n");
-    //task_yield();
-    uart_puts("[Task 0] I am back\n");
+    // task_yield();
+    // uart_puts("[Task 0] I am back\n");
+
+    struct timer *t1 = timer_create(timer_func, &person, 3);
+	if (NULL == t1) {
+		printf("timer_create() failed!\n");
+	}
+	struct timer *t2 = timer_create(timer_func, &person, 5);
+	if (NULL == t2) {
+		printf("timer_create() failed!\n");
+	}
+	struct timer *t3 = timer_create(timer_func, &person, 8);
+	if (NULL == t3) {
+		printf("timer_create() failed!\n");
+	}
+
     while (1)
     {
         uart_puts("[Task 0] Running...\n");
-        #ifdef USE_LOCK
-        spin_lock();
-        uart_puts("[Task 0] Required Lock\n");
-        #endif
-        for(int i = 0; i < 5; i++)
-        {
-            uart_puts("[Task 0] Critical section...\n");
-            task_delay(DELAY);
-        }
-        #ifdef USE_LOCK
-        uart_puts("[Task 0] Release Lock\n");
-		spin_unlock();
+        // #ifdef USE_LOCK
+        // spin_lock();
+        // uart_puts("[Task 0] Required Lock\n");
+        // #endif
+        // for(int i = 0; i < 5; i++)
+        // {
+        //     uart_puts("[Task 0] Critical section...\n");
+        //     task_delay(DELAY);
+        // }
+        // #ifdef USE_LOCK
+        // uart_puts("[Task 0] Release Lock\n");
+		// spin_unlock();
 
-        #endif
+        // #endif
         task_delay(DELAY);        
     }
 }
